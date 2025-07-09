79c66d0471e43b019f0782fe0b8252e29568f151
hanno <hcoetzer@sts3d.co.za>
2025-07-09 10:42:00 +0200

    grep live changes to not ignore .gitignore and ignore .git

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -12,6 +12,24 @@ vim.keymap.set("n", "<F11>", dap.step_into, { desc = "Debug: Step Into" })
 vim.keymap.set("n", "<F12>", dap.step_out, { desc = "Debug: Step Out" })
 vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint, { desc = "Debug: Toggle Breakpoint" })
 vim.keymap.set("n", "<leader>du", dapui.toggle, { desc = "Debug: Toggle UI" })
+vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>", { desc = "Toggle Neo-tree" })
 vim.keymap.set({ "n", "x" }, "<leader>ca", function()
   require("tiny-code-action").code_action()
-end, { noremap = true, silent = true })
+end, { desc = "Toggle tiny code actions", noremap = true, silent = true })
+local live_grep_custom = function()
+  require("telescope.builtin").live_grep({
+    additional_args = function()
+      return {
+        "--no-ignore",
+        "--hidden",
+        "--glob",
+        "!**/.git/*",
+        "--glob",
+        "!**/.git/**",
+      }
+    end,
+  })
+end
+
+vim.keymap.set("n", "<leader>sg", live_grep_custom, { desc = "Live Grep (skip .git)" })
+vim.keymap.set("n", "<leader>/", live_grep_custom, { desc = "Live Grep (quick)" })
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,17 +1,13 @@
--- ~/.config/nvim/lua/plugins/neo-tree.lua
 return {
   "nvim-neo-tree/neo-tree.nvim",
-  keys = {
-    { "<leader>e", "<cmd>Neotree toggle<CR>", desc = "Toggle Neo-tree" },
-  },
   opts = {
     filesystem = {
       filtered_items = {
-        visible = true, -- Show hidden items
-        hide_dotfiles = false, -- Do not hide .files
-        hide_gitignored = false, -- Do not hide gitignored files
+        visible = true, -- Show filtered (hidden) files
+        hide_dotfiles = false,
+        hide_gitignored = false,
         hide_by_name = {},
-        never_show = {},
+        never_show = {}, -- Nothing should be excluded
       },
     },
   },
